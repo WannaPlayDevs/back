ca8b220a43cdc971cecb270bbd21410c3bc99f46
Sergio <Sergio@iMac-de-Sergio.local>
2018-06-01 19:52:33 +0200

    añadida la opcion de update user

--- wannaplay/usuarios/schema.py
+++ wannaplay/usuarios/schema.py
@@ -42,19 +42,109 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user)
 
 
-class DeleteUser(graphene.ClientIDMutation):
-    # permission_classes = (SomePermissionClass,)
+class UpdateUser(graphene.Mutation):
+    pkUser = graphene.Int()
+    username = graphene.String()
+    password = graphene.String()
+    alias = graphene.String()
+    karma = graphene.Int()
+    steamName = graphene.String()
+    bnetName = graphene.String()
+    horarioManana = graphene.Boolean()
+    horarioTarde = graphene.Boolean()
+    horarioNoche = graphene.Boolean()
+    horarioMadrugada = graphene.Boolean()
+    playOverwatch = graphene.Boolean()
+    playWow = graphene.Boolean()
+    playRust = graphene.Boolean()
+    playArk = graphene.Boolean()
+    playGta = graphene.Boolean()
+    playPubg = graphene.Boolean()
+    playFortnite = graphene.Boolean()
 
-    class Input:
-        id = graphene.String()
+    class Arguments:
+        pkUser = graphene.Int()
+        username = graphene.String()
+        password = graphene.String()
+        alias = graphene.String()
+        karma = graphene.Int()
+        steamName = graphene.String()
+        bnetName = graphene.String()
+        horarioManana = graphene.Boolean()
+        horarioTarde = graphene.Boolean()
+        horarioNoche = graphene.Boolean()
+        horarioMadrugada = graphene.Boolean()
+        playOverwatch = graphene.Boolean()
+        playWow = graphene.Boolean()
+        playRust = graphene.Boolean()
+        playArk = graphene.Boolean()
+        playGta = graphene.Boolean()
+        playPubg = graphene.Boolean()
+        playFortnite = graphene.Boolean()
 
-    @classmethod
-    def mutate_and_get_payload(cls, input, context, info):
-        User.objects.get(pk=pkUser(input.get('id'))[1]).delete()
-        return DeleteUser()
+    def mutate(self, info, pkUser, username=None, password=None, alias=None,
+               karma=None, steamName=None, bnetName=None, horarioManana=None,
+               horarioTarde=None, horarioNoche=None, horarioMadrugada=None,
+               playOverwatch=None, playWow=None, playRust=None, playArk=None,
+               playGta=None, playPubg=None, playFortnite=None):
+        user = User.objects.get(pkUser=pkUser)
+
+        if (username is not None):
+            user.username = username
+            user.save()
+        if (password is not None):
+            user.password = password
+            user.save()
+        if (alias is not None):
+            user.alias = alias
+            user.save()
+        if (karma is not None):
+            user.karma = karma
+            user.save()
+        if (steamName is not None):
+            user.steamName = steamName
+            user.save()
+        if (bnetName is not None):
+            user.bnetName = bnetName
+            user.save()
+        if (horarioManana is not None):
+            user.horarioManana = horarioManana
+            user.save()
+        if (horarioTarde is not None):
+            user.horarioTarde = horarioTarde
+            user.save()
+        if (horarioNoche is not None):
+            user.horarioNoche = horarioNoche
+            user.save()
+        if (horarioMadrugada is not None):
+            user.horarioMadrugada = horarioMadrugada
+            user.save()
+        if (playOverwatch is not None):
+            user.playOverwatch = playOverwatch
+            user.save()
+        if (playWow is not None):
+            user.playWow = playWow
+            user.save()
+        if (playRust is not None):
+            user.playRust = playRust
+            user.save()
+        if (playArk is not None):
+            user.playArk = playArk
+            user.save()
+        if (playGta is not None):
+            user.playGta = playGta
+            user.save()
+        if (playPubg is not None):
+            user.playPubg = playPubg
+            user.save()
+        if (playFortnite is not None):
+            user.playFortnite = playFortnite
+            user.save()
+
+        return user
+
 
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
-    delete_user = DeleteUser.Field()
-
+    update_user = UpdateUser.Field()
